9b548ada1f2a87e22b44f61a0afd909aa27e650a
Laura Messa Travin <laura.travin@gmail.com>
2018-11-07 04:26:56 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -66,7 +66,7 @@ class InteractiveRecord
          k = search.keys[0].to_s
          v = search[search.keys[0]]
       binding.pry
-      sql = "SELECT * FROM #{self.table_name} WHERE #{k} = #{v}"
+      sql = "SELECT * FROM #{self.table_name} WHERE #{k} = '#{v}'"
       binding.pry
       DB[:conn].execute(sql)
     end
